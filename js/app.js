493c2671b1c9f1aef6fd0401402d53767c819108
Bruno Lima <brunolimawd@gmail.com>
2014-07-24 15:28:06 -0300

    Get gelocation ok

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
   var translate = navigator.mozL10n.get;
   var buttonFindMe = document.getElementById('button-reload');
-  var clientIdInstagram = '310accf4e9724819bb42902d1634d774';
-  var apiURL = 'https://api.instagram.com/v1/media/search?client_id=' + clientIdInstagram;
+  var apiMaps = 'https://maps.googleapis.com/maps/api/geocode/json?';
   var errorMsg = document.getElementById('error');
   var results = document.getElementById('results');
   var request = null;
@@ -64,56 +63,6 @@ window.addEventListener('DOMContentLoaded', function() {
   }, false);
 
 
-  // Convert created time to time ago
-  function getRelativeTime(time) {
-    var parsed_date = new Date (time * 1000);
-    // Defines relative to what ... default is now
-    var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-    var delta = parseInt((relative_to.getTime()-parsed_date)/1000);
-    delta=(delta<2)?2:delta;
-    var timeAgo = '';
-
-    // Parse delta
-    if (delta < 60) {
-      timeAgo = delta + ' ' + translate('seconds_ago');
-    } else if(delta < 120) {
-      timeAgo = translate('a_minute_ago');
-    } else if(delta < (45*60)) {
-      timeAgo = (parseInt(delta / 60, 10)).toString() + ' ' + translate('minutes_ago');
-    } else if(delta < (2*60*60)) {
-      timeAgo = translate('an_hour_ago');
-    } else if(delta < (24*60*60)) {
-      timeAgo = '' + (parseInt(delta / 3600, 10)).toString() + ' ' + translate('hours_ago');
-    } else if(delta < (48*60*60)) {
-      timeAgo = translate('a_day_ago');
-    } else {
-      timeAgo = (parseInt(delta / 86400, 10)).toString() + ' ' + translate('days_ago');
-    }
-
-    return timeAgo;
-  };
-
-
-  // Web activity for share url photo
-  function sharePhoto(photoUrl){
-    var activityShare = new MozActivity({
-      name: 'share',
-      data: {
-        type: 'url',
-        url: photoUrl
-      }
-    });
-
-    activityShare.onsuccess = function() {
-      console.log('Success');
-    };
-
-    activityShare.onerror = function() {
-      console.log(this.error);
-    };
-  };  
-
-
   // Get photos by location on Instagram
   function search() {
     // Are we searching already? Then stop that search
@@ -127,8 +76,10 @@ window.addEventListener('DOMContentLoaded', function() {
     errorMsg.className = 'hidden';
 
     // Creat url for request
-    var term = '&lat=' + latitude + '&lng=' + longitude + '&distance=' + distance;
-    var url = apiURL + term;
+    //var term = 'latlng=' + latitude + ',' + longitude;
+    var term = 'latlng=-8.069443,-34.899722';
+    var url = apiMaps + term;
+    console.log(url);
 
     request = new XMLHttpRequest({ mozSystem: true });
     request.open('get', url, true);
@@ -162,75 +113,75 @@ window.addEventListener('DOMContentLoaded', function() {
     results.textContent = '';
     results.className = '';
 
-    var photos = response.data;
-    
-    if(photos.length === 0) {
-      results.textContent = translate('search_no_results');
-      results.className = 'searching';
-      errorMsg.className = 'hidden';
+    var city = response.results[0].address_components[3].long_name;
+    var street = response.results[0].address_components[1].long_name;
+
+    console.log(street + ', ' + city);
+
+    if(city == 'Recife') {
+
     } else {
-      var listPhotos = document.createElement('ul');
-      results.appendChild(listPhotos);
-
-      photos.forEach(function(item) {
-        // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
-        // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
-        var liPhoto = document.createElement('li');
-        var header = document.createElement('header');
-        var profileAvatar = document.createElement('img');
-        var profileName = document.createElement('h2');
-        var timeAgo = document.createElement('span');
-        var wrapPhoto = document.createElement('figure');
-        var photo = document.createElement('img');
-        var photoCaption = document.createElement('figcaption');
-        var photoMenu = document.createElement('menu');
-        var liShare = document.createElement('li');
-        var buttonSahre = document.createElement('button');
-
-        // Set values
-        liPhoto.className = 'photo';
-        profileAvatar.src = item.user.profile_picture;
-        profileAvatar.alt = item.user.username;
-        profileName.textContent = item.user.full_name;
-        timeAgo.className = 'time-ago';
-        timeAgo.textContent = getRelativeTime(item.created_time);
-        photo.src = item.images.low_resolution.url;
-        if ( item.caption ) {
-          photoCaption.textContent = item.caption.text;  
-        }
-        photo.alt = 'Photo by ' + item.user.username;        
-        photoMenu.type = 'toolbar';
-        buttonSahre.className = 'button-share';
-        buttonSahre.value = item.link;
-        buttonSahre.textContent = 'Share';
-
-        // Creat photo template 
-        header.appendChild(profileAvatar);
-        header.appendChild(profileName);
-        header.appendChild(timeAgo);
-        
-        wrapPhoto.appendChild(photo);
-        wrapPhoto.appendChild(photoCaption);
-
-        liShare.appendChild(buttonSahre);
-        photoMenu.appendChild(liShare);
-        
-        liPhoto.appendChild(header);
-        liPhoto.appendChild(wrapPhoto);
-        liPhoto.appendChild(photoMenu);
-
-        listPhotos.appendChild(liPhoto);
-
-        // Convert emoji
-        profileName.innerHTML = emoji.a(profileName.innerHTML);
-        photoCaption.innerHTML = emoji.a(photoCaption.innerHTML);
-
-        // Listening share button
-        buttonSahre.addEventListener('click', function(e) {
-            sharePhoto(this.value);
-        }, false);
-      });
+      showError(translate('city_error'));
     }
+    
+    // if(photos.length === 0) {
+    //   results.textContent = translate('search_no_results');
+    //   results.className = 'searching';
+    //   errorMsg.className = 'hidden';
+    // } else {
+    //   var listPhotos = document.createElement('ul');
+    //   results.appendChild(listPhotos);
+
+    //   photos.forEach(function(item) {
+    //     // We're using textContent because inserting content from external sources into your page using innerHTML can be dangerous.
+    //     // https://developer.mozilla.org/Web/API/Element.innerHTML#Security_considerations
+    //     var liPhoto = document.createElement('li');
+    //     var header = document.createElement('header');
+    //     var profileAvatar = document.createElement('img');
+    //     var profileName = document.createElement('h2');
+    //     var timeAgo = document.createElement('span');
+    //     var wrapPhoto = document.createElement('figure');
+    //     var photo = document.createElement('img');
+    //     var photoCaption = document.createElement('figcaption');
+    //     var photoMenu = document.createElement('menu');
+    //     var liShare = document.createElement('li');
+    //     var buttonSahre = document.createElement('button');
+
+    //     // Set values
+    //     liPhoto.className = 'photo';
+    //     profileAvatar.src = item.user.profile_picture;
+    //     profileAvatar.alt = item.user.username;
+    //     profileName.textContent = item.user.full_name;
+    //     timeAgo.className = 'time-ago';
+    //     photo.src = item.images.low_resolution.url;
+    //     if ( item.caption ) {
+    //       photoCaption.textContent = item.caption.text;  
+    //     }
+    //     photo.alt = 'Photo by ' + item.user.username;        
+    //     photoMenu.type = 'toolbar';
+    //     buttonSahre.className = 'button-share';
+    //     buttonSahre.value = item.link;
+    //     buttonSahre.textContent = 'Share';
+
+    //     // Creat photo template 
+    //     header.appendChild(profileAvatar);
+    //     header.appendChild(profileName);
+    //     header.appendChild(timeAgo);
+        
+    //     wrapPhoto.appendChild(photo);
+    //     wrapPhoto.appendChild(photoCaption);
+
+    //     liShare.appendChild(buttonSahre);
+    //     photoMenu.appendChild(liShare);
+        
+    //     liPhoto.appendChild(header);
+    //     liPhoto.appendChild(wrapPhoto);
+    //     liPhoto.appendChild(photoMenu);
+
+    //     listPhotos.appendChild(liPhoto);
+
+    //   });
+    // }
 
     results.className = '';
   };
